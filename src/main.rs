0ebc299f52cb8b9ca926a09a02fe8a945a7fe96b
karthik-k-multipl <kartik.kalarikal@multipl.xyz>
2025-04-22 23:06:28 +0530

    cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::{env, net::IpAddr,str::FromStr};
+
+
+
+struct Arguments{
+    thread: u16,
+    ip_addr: IpAddr,
+    flag: String
+}
+
+impl Arguments{
+    fn new(args: &[String])->Result<Arguments,&'static str>{
+        if args.len()<2{
+            return &Err("not enough arguments");
+        }else if args.len()>4{
+            return  &Err("too many arguments");
+        }
+
+        let f = args[1].clone(); 
+
+        if let Ok(ip_addr) = IpAddr::from_str(&f){
+            return Ok(Arguments { thread: 4, ip_addr: ip_addr, flag: String::from("")});
+        }else{
+            let flag =args[1].clone();
+            if flag.contains("-h") || flag.contains("--help")&&args.len()==2{
+                println!("-j for selecting how many threads for operation");
+            }
+            return Err("help");
+        }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+
+    let args:Vec<String> = env::args().collect();
+
+    for i in args{
+        println!("{}",i);
+    }
 }
